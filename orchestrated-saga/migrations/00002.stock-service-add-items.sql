f85da1d8b8d39120fc082cf927d09572a88da548
DevVictor19 <antoniovictor12@live.com>
2026-02-22 18:45:58 -0300

    feat: replace UUID generation with static values for stock items

--- orchestrated-saga/migrations/00002.stock-service-add-items.sql
+++ orchestrated-saga/migrations/00002.stock-service-add-items.sql
@@ -1,12 +1,12 @@
 INSERT INTO stock_service.item (uuid, name, description, price, quantity_in_stock)
 VALUES
-  (gen_random_uuid(), 'Notebook', 'Notebook Dell Inspiron 15', 350000, 20),
-  (gen_random_uuid(), 'Smartphone', 'Smartphone Samsung Galaxy S21', 250000, 35),
-  (gen_random_uuid(), 'Monitor', 'Monitor LG 24 polegadas', 90000, 15),
-  (gen_random_uuid(), 'Mouse', 'Mouse Logitech MX Master 3', 35000, 50),
-  (gen_random_uuid(), 'Teclado', 'Teclado Mecânico Redragon', 45000, 40),
-  (gen_random_uuid(), 'Headset', 'Headset HyperX Cloud II', 60000, 25),
-  (gen_random_uuid(), 'Impressora', 'Impressora HP LaserJet', 120000, 10),
-  (gen_random_uuid(), 'Tablet', 'Tablet Apple iPad 10th Gen', 180000, 18),
-  (gen_random_uuid(), 'Cadeira Gamer', 'Cadeira Gamer DXRacer', 80000, 12),
-  (gen_random_uuid(), 'Webcam', 'Webcam Logitech C920', 30000, 22);
+  ('01094d51-0e14-46a9-a9a5-ff8710666ba6', 'Notebook', 'Notebook Dell Inspiron 15', 350000, 20),
+  ('a3b5127d-18b1-4b1e-9ccf-a34b60f6ca87', 'Smartphone', 'Smartphone Samsung Galaxy S21', 250000, 35),
+  ('9662a5ef-f1e7-4332-8f91-50722ef952e9', 'Monitor', 'Monitor LG 24 polegadas', 90000, 15),
+  ('6173694d-21b3-4018-b39d-e6a2e8bae02f', 'Mouse', 'Mouse Logitech MX Master 3', 35000, 50),
+  ('d2b0e250-1c35-467c-9c27-9708ef78fd4b', 'Teclado', 'Teclado Mecânico Redragon', 45000, 40),
+  ('631a0555-f3e2-4e85-a340-82a1173120f4', 'Headset', 'Headset HyperX Cloud II', 60000, 25),
+  ('0a303f7e-e56c-48fd-8d8d-1fab9145bfe0', 'Impressora', 'Impressora HP LaserJet', 120000, 10),
+  ('3d217dbf-323f-42d7-a85b-91a5db5e4eec', 'Tablet', 'Tablet Apple iPad 10th Gen', 180000, 18),
+  ('b17a3d78-614b-4c53-9c8a-2c2da9d92327', 'Cadeira Gamer', 'Cadeira Gamer DXRacer', 80000, 12),
+  ('4919fa28-41f7-493f-90b1-070c4e57f49e', 'Webcam', 'Webcam Logitech C920', 30000, 22);
