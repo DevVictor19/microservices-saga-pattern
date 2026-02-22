5ceffb60b9b4ab9df2c0cf0f5349390a06d52cb6
DevVictor19 <antoniovictor12@live.com>
2026-02-22 11:25:37 -0300

    feat: add stock service

---
+++ orchestrated-saga/stock-service/src/app.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CoreModule } from './modules/core/core.module';
+import { Item, ItemDelivery, ItemReservation } from './modules/core/entities';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST || 'localhost',
+      port: Number(process.env.DB_PORT) || 5432,
+      username: process.env.DB_USERNAME || 'postgres',
+      password: process.env.DB_PASSWORD || 'password',
+      schema: process.env.DB_SCHEMA || 'stock_service',
+      entities: [Item, ItemReservation, ItemDelivery],
+    }),
+    CoreModule,
+  ],
+})
+export class AppModule {}
---
+++ orchestrated-saga/stock-service/src/modules/core/entities/item-delivery.entity.ts
@@ -0,0 +1,24 @@
+import { BaseEntity } from 'src/@shared';
+import { Column, Entity, JoinColumn, ManyToOne } from 'typeorm';
+import { Item } from './item.entity';
+
+@Entity()
+export class ItemDelivery extends BaseEntity {
+  @JoinColumn({ name: 'item_id' })
+  itemId: number;
+
+  @ManyToOne(() => Item, { nullable: false })
+  item: Item;
+
+  @Column({ name: 'user_uuid', type: 'uuid', nullable: false })
+  userUuid: string;
+
+  @Column({ name: 'order_uuid', type: 'uuid', nullable: false })
+  orderUuid: string;
+
+  @Column({ name: 'payment_uuid', type: 'uuid', nullable: false })
+  paymentUuid: string;
+
+  @Column({ name: 'delivery_forecast', type: 'timestamp', nullable: false })
+  deliveryForecast: Date;
+}
