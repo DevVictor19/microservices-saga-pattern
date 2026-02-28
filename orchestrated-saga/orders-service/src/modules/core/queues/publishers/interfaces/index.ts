22f5c3548b7ddf3bc7567b752b0e4134e915a18d
DevVictor19 <antoniovictor12@live.com>
2026-02-28 08:45:03 -0300

    feat(orders-service): export additional publisher interfaces for order processing

--- orchestrated-saga/orders-service/src/modules/core/queues/publishers/interfaces/index.ts
+++ orchestrated-saga/orders-service/src/modules/core/queues/publishers/interfaces/index.ts
@@ -1,2 +1,5 @@
 export * from './order-items-reservation-publisher.interface';
 export * from './order-payment-publisher.interface';
+export * from './order-items-undo-reservation-publisher.interface';
+export * from './order-send-to-deliver-publisher.interface';
+export * from './order-receive-loyalty-points-publisher.interface';
