5a502a254f34decd6ff09846f64bae976826a5e4
DevVictor19 <antoniovictor12@live.com>
2026-02-28 18:17:35 -0300

    feat(loyalty-service): add persistence and queues setup

--- orchestrated-saga/loyalty-service/src/app.module.ts
+++ orchestrated-saga/loyalty-service/src/app.module.ts
@@ -1,4 +1,26 @@
+import { BullModule } from '@nestjs/bullmq';
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CoreModule } from './modules/core/core.module';
 
-@Module({})
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST || 'localhost',
+      port: Number(process.env.DB_PORT) || 5432,
+      username: process.env.DB_USERNAME || 'postgres',
+      password: process.env.DB_PASSWORD || 'password',
+      schema: process.env.DB_SCHEMA || 'loyalty_service',
+      entities: [],
+    }),
+    BullModule.forRoot({
+      connection: {
+        host: process.env.REDIS_HOST || 'localhost',
+        port: Number(process.env.REDIS_PORT) || 6379,
+      },
+    }),
+    CoreModule,
+  ],
+})
 export class AppModule {}
