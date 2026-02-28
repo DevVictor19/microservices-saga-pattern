43f0d7e00f6921d40ae76b29c27f48f4198424f3
DevVictor19 <antoniovictor12@live.com>
2026-02-28 08:45:09 -0300

    feat(orders-service): re-export existing publishers in index file

--- orchestrated-saga/orders-service/src/modules/core/queues/publishers/index.ts
+++ orchestrated-saga/orders-service/src/modules/core/queues/publishers/index.ts
@@ -1,3 +1,6 @@
 export * from './interfaces';
 export * from './order-items-reservation.publisher';
 export * from './order-payment.publisher';
+export * from './order-send-to-deliver.publisher';
+export * from './order-items-undo-reservation.publisher';
+export * from './order-receive-loyalty-points.publisher';
