56ad1287e76470256c0bb821f2c8325c2117551b
DevVictor19 <antoniovictor12@live.com>
2026-02-28 12:56:09 -0300

    feat(core-module): configure BullMQ queues and update providers in CoreModule

--- orchestrated-saga/payments-service/src/modules/core/core.module.ts
+++ orchestrated-saga/payments-service/src/modules/core/core.module.ts
@@ -1,8 +1,41 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Payment } from './entities';
+import { BullModule } from '@nestjs/bullmq';
+import {
+  ORDER_PAYMENT_QUEUE,
+  ORDER_PAYMENT_RESULT_QUEUE,
+  OrderPaymentConsumer,
+  OrderPaymentResultPublisher,
+  OrderPaymentResultPublisherImpl,
+} from './queues';
+import { PaymentRepository, PaymentRepositoryImpl } from './repositories';
+import { PaymentService, PaymentServiceImpl } from './services';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Payment])],
+  imports: [
+    TypeOrmModule.forFeature([Payment]),
+    BullModule.registerQueue({
+      name: ORDER_PAYMENT_QUEUE,
+    }),
+    BullModule.registerQueue({
+      name: ORDER_PAYMENT_RESULT_QUEUE,
+    }),
+  ],
+  providers: [
+    {
+      provide: PaymentRepository,
+      useClass: PaymentRepositoryImpl,
+    },
+    {
+      provide: PaymentService,
+      useClass: PaymentServiceImpl,
+    },
+    {
+      provide: OrderPaymentResultPublisher,
+      useClass: OrderPaymentResultPublisherImpl,
+    },
+    OrderPaymentConsumer,
+  ],
 })
 export class CoreModule {}
